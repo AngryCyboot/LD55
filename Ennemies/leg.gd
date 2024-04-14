013ae3af320bc3583a508103abc86796e738ee0d
AngryCyboot <cyboot25@gmail.com>
2024-04-14 19:05:28 +0200

    fix of the floaty legs

--- Ennemies/leg.gd
+++ Ennemies/leg.gd
@@ -3,14 +3,16 @@ class_name Leg
 
 @export var max_speed : float = 10
 @export var max_extention : float = 4
+@export var close_enough : float = 0.5
 
 @onready var visual : MeshInstance3D = $LegVisual
 @onready var parent : Node3D = get_parent()
+@onready var boss : Node3D = $"../../.."
 
-var mobile : bool = true
+var is_mobile : bool = true
 var speed : float = 0
 var rest_pos :Node3D
-var last_pos : Vector3
+
 
 func _ready():
 	rest_pos = Node3D.new()
@@ -21,27 +23,45 @@ func _ready():
 
 func _process(delta):
 	var extention : float = global_position.distance_to(rest_pos.global_position)
-	if extention> max_extention*1.1:
-		mobile = true
-		visual.set_instance_shader_parameter("mobile",mobile)
-	if mobile:
-		step($"../../.."._target_node.global_position,delta)
-		last_pos = global_position
+	if extention> max_extention+close_enough*2 and not is_mobile:
+		mobile(true)
+		print("max")
+	if is_mobile:
+		step(compute_foot_target(),delta)
 		var angle : float = atan2(parent.global_position.x-global_position.x,parent.global_position.z-global_position.z)
 		rotation.y = lerp_angle(rotation.y,angle,delta)
 
-func step(target : Vector3,delta : float) -> void:
-	var foot_target :Vector3 = $"..".global_position.direction_to(target)
-	foot_target = foot_target * max_extention + rest_pos.global_position
+
+func step(foot_target : Vector3,delta : float) -> void:
 	var dist : float = foot_target.distance_to(Vector3(global_position.x,0,global_position.z))
-	if dist > 0.1:
-		var target_height : float = dist*2
+	if dist > close_enough or global_position.y > close_enough/2:
+		var target_height : float = dist
+		if target_height > max_extention : target_height=max_extention
 		if dist*2> speed and speed < max_speed:
-			speed += (max_speed-speed)*delta*2
-		elif speed > dist*2 and speed > 5:
-			speed -= speed*delta*2
-		var dir : Vector3 = Vector3(foot_target.x,target_height,foot_target.z)
-		global_position = global_position.move_toward(dir,delta*speed)
+			speed += (max_speed-speed)*delta
+		elif speed > dist*2 and speed > max_speed/2:
+			speed -= speed*delta
+		global_position.y += (target_height-global_position.y)*delta*speed
+		var rotation_radius : float = boss.global_position.distance_to(global_position)
+		var rotation_speed : float = tan(deg_to_rad(boss.angular_velocity.y)) * rotation_radius *15
+		var actual_speed : float = speed + boss.linear_velocity.length() + rotation_speed**2
+		var dir : Vector3 = Vector3(foot_target.x,0,foot_target.z)
+		global_position = global_position.move_toward(dir,delta*actual_speed)
 	else:
-		mobile = false
-		visual.set_instance_shader_parameter("mobile",mobile)
+		mobile(false)
+
+func compute_foot_target(target: Vector3 = boss._target_node.global_position) -> Vector3:
+	var foot_target: Vector3 = $"..".global_position.direction_to(target)
+	foot_target = foot_target * (max_extention-close_enough) + rest_pos.global_position
+	foot_target.y = 0
+	$debug.global_position = foot_target
+	return foot_target
+
+func mobile(x : bool)-> void:
+	if x != is_mobile:
+		is_mobile = x
+		visual.set_instance_shader_parameter("mobile",is_mobile)
+		if is_mobile :
+			parent.mobile += 1
+		else:
+			parent.mobile -= 1
---
+++ Ennemies/legs_manager.gd
@@ -0,0 +1,23 @@
+extends Node3D
+
+var legs : Array[Leg]
+var mobile : int = 0
+
+func _ready():
+	for leg in get_children():
+		if leg is Leg : legs.append(leg)
+	mobile = legs.size()
+
+func _process(delta):
+	var max_mobile : int = legs.size()/2
+	if mobile < max_mobile and $"../.."._target_node.global_position.distance_to(global_position)>1:
+		var dist : float = -1
+		var furthest : Leg
+		for leg in legs:
+			var wish_dist : float = leg.compute_foot_target().distance_to(leg.global_position)
+			if wish_dist>dist: 
+				dist = wish_dist
+				furthest=leg
+		if dist > furthest.max_extention : 
+			furthest.mobile(true)
+			print("managed")
