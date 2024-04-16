af42577393471d2e2194632cda8876f8c35daf7b
AngryCyboot <cyboot25@gmail.com>
2024-04-16 03:13:15 +0200

    removing sbires

--- Main.gd
+++ Main.gd
@@ -14,18 +14,18 @@ func _process(delta):
 	timer += delta
 	if timer > spawn_time :
 		var boss : Boss = boss_scene.instantiate()
-		var i : int = randi_range(3,5)
+		#var i : int = randi_range(3,5)
 		var angle : float = randf_range(0,TAU)
 		boss.position = Vector3(cos(angle),0,sin(angle))*spawn_radius
 		boss.rotate_y(-angle)
 		$Enemies.add_child(boss)
-		for x in i:
-			angle = float(x/float(i)) * TAU
-			var sbire : NavAgent = sbire_scene.instantiate()
-			boss.sbires.append(sbire)
-			sbire._my_boss = boss
-			var pos = Vector3(cos(angle),0,sin(angle))*60
-			boss.add_sibling(sbire)
-			sbire.global_position = pos + boss.global_position
+		#for x in i:
+			#angle = float(x/float(i)) * TAU
+			#var sbire : NavAgent = sbire_scene.instantiate()
+			#boss.sbires.append(sbire)
+			#sbire._my_boss = boss
+			#var pos = Vector3(cos(angle),0,sin(angle))*60
+			#boss.add_sibling(sbire)
+			#sbire.global_position = pos + boss.global_position
 		_char.boss_spawned(boss)
 		timer = 0
